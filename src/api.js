4e3c4eb1d947eb83315f1367089c17b87e241b5a
Minora, Leonardo <leonardo.minora@gmail.com>
2015-06-11 13:49:12 -0300

    Implementado a tarefa #2 Changes to be committed: 	new file:   npm-debug.log 	modified:   package.json 	modified:   src/api.js 	modified:   test/api-test.js

--- src/api.js
+++ src/api.js
@@ -1,15 +1,34 @@
-var express = require('express');
-var app = express();
+// #!/usr/bin/env node --harmony
+// 'use strict';
+
+const express = require('express');
+const app = express();
+
+// app.use(express.logger('dev'));
 
 app.get('/', function (req, res) {
-  res.send('OK');
+    res.send('API do jogo Aliado');
+});
+
+app.get('/api/:tabuleiro/', function(req, res) {
+    res.status(200).json({
+        "tabuleiro" : {
+            "jogadores" : {
+                "0000" : {"Ficha1" : [0,0],"Ficha2" : [0,0],"Ficha3" : [0,0],"Ficha4" : [0,0]},
+                "0018" : {"Ficha1" : [0,18],"Ficha2" : [0,18],"Ficha3" : [0,18],"Ficha4" : [0,18]},
+                "1818" : {"Ficha1" : [18,18],"Ficha2" : [18,18],"Ficha3" : [0,0],"Ficha4" : [18,18]},
+                "1800" : {"Ficha1" : [18,0],"Ficha2" : [18,0],"Ficha3" : [18,0],"Ficha4" : [18,0]}
+            },
+            "nome" : req.params.tabuleiro,
+            "rodadas": "1",
+            "jogador_da_vez": "0000"
+        }
+    });
 });
 
 var server = app.listen(3000, function () {
+    const host = server.address().address
+    const port = server.address().port;
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('Jogo Aliado... listening at http://%s:%s', host, port);
 });
--- test/api-test.js
+++ test/api-test.js
@@ -1,16 +1,73 @@
 var request = require('supertest')
+const url = 'http://localhost:3000';
 
 describe('Teste de ambiente', function () {
-
+	var path = '/';
   it('Deve retornar estado 200 na URL /', function (done) {
-
-    request('http://localhost:3000')
-      .get('/')
+    request(url)
+      .get(path)
       .expect(200)
       .end(function(err, res){
         if (err) return done(err)
         done()
       })
-
   })
+})
+
+describe('Testando o estado de um jogo', function() {
+	var path = '/api/';
+
+	it('Teste de resposta 200 do jogo exemplo', function(done) {
+		request(url)
+		 	.get(path + 'exemplo/')
+		 	.expect(200)
+		 	.end(function(error, response) {
+		 		if (error) return done(error)
+		 		done()
+		 	})
+	})
+
+	it('Teste de resposta do estado do jogo exemplo', function(done) {
+		request(url)
+		 	.get(path + 'exemplo/')
+		 	.expect({
+    "tabuleiro" : {
+        "jogadores" : {
+            "0000" : {"Ficha1" : [0,0],"Ficha2" : [0,0],"Ficha3" : [0,0],"Ficha4" : [0,0]},
+            "0018" : {"Ficha1" : [0,18],"Ficha2" : [0,18],"Ficha3" : [0,18],"Ficha4" : [0,18]},
+            "1818" : {"Ficha1" : [18,18],"Ficha2" : [18,18],"Ficha3" : [0,0],"Ficha4" : [18,18]},
+            "1800" : {"Ficha1" : [18,0],"Ficha2" : [18,0],"Ficha3" : [18,0],"Ficha4" : [18,0]}
+        },
+        "nome" : "exemplo",
+        "rodadas": "1",
+        "jogador_da_vez": "0000"
+    }
+})
+		 	.end(function(error, response) {
+		 		if (error) return done(error)
+		 		done()
+		 	})
+	})
+
+	it('Teste de resposta do estado do jogo meu-exemplo', function(done) {
+		request(url)
+		 	.get(path + 'meu-exemplo/')
+		 	.expect({
+    "tabuleiro" : {
+        "jogadores" : {
+            "0000" : {"Ficha1" : [0,0],"Ficha2" : [0,0],"Ficha3" : [0,0],"Ficha4" : [0,0]},
+            "0018" : {"Ficha1" : [0,18],"Ficha2" : [0,18],"Ficha3" : [0,18],"Ficha4" : [0,18]},
+            "1818" : {"Ficha1" : [18,18],"Ficha2" : [18,18],"Ficha3" : [0,0],"Ficha4" : [18,18]},
+            "1800" : {"Ficha1" : [18,0],"Ficha2" : [18,0],"Ficha3" : [18,0],"Ficha4" : [18,0]}
+        },
+        "nome" : "meu-exemplo",
+        "rodadas": "1",
+        "jogador_da_vez": "0000"
+    }
+})
+		 	.end(function(error, response) {
+		 		if (error) return done(error)
+		 		done()
+		 	})
+	})
 })
