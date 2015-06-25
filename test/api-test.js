c6e41afbb03990780cfb1f396b02fa5774a0a961
Minora, Leonardo <leonardo.minora@gmail.com>
2015-06-25 13:50:30 -0300

    issue #7 substituido o pacote de requisição e teste (supertest -> request+should) Changes to be committed: 	modified:   test/api-test.js

--- test/api-test.js
+++ test/api-test.js
@@ -1,85 +1,80 @@
-var request = require('supertest')
+// const request = require('supertest')
+const request = require('request');
+const assert = require("should");
+
 const url = 'http://localhost:3000';
 
-describe('Teste de ambiente', function () {
-	var path = '/';
-  it('Deve retornar estado 200 na URL /', function (done) {
-    request(url)
-      .get(path)
-      .expect(200)
-      .end(function(err, res){
-        if (err) return done(err)
-        done()
-      })
-  })
-})
-
-describe('Testando o estado de um jogo', function() {
-	var path = '/api/';
-	var tabuleiro_nome = 'exemplo';
-
-	it('Teste de resposta 404 do jogo exemplo nao criado', function(done) {
-		request(url)
-		 	.get(path + tabuleiro_nome + '/')
-		 	.expect(404)
-		 	.end(function(error, response) {
-		 		if (error) return done(error)
-		 		done()
-		 	})
-	})
-
-	it('Teste de resposta 200 do estado do jogo ao criar um novo jogo', function(done) {
-		var acao = '/novo';
-
-		request(url)
-			.get(path + tabuleiro_nome + acao)
-			.expect(200)
-			.end(function(error, response) {
-				if (error) return done(error);
+describe('Teste da API', function() {
+	describe('Teste de ambiente', function () {
+		var path = '/';
+		it('Deve retornar estado 200 na URL /', function (done) {
+			request(url, function(error, response, body) {
+				(error==null).should.be.true;
+				(response.statusCode).should.be.equal(200);
 				done();
 			});
-
+		});
 	});
 
-	it('Teste de resposta do estado do jogo ao criar um novo jogo JSON', function(done) {
-		var acao = '/';
-		var estado_esperado = JSON.stringify({
-         'nome': 'exemplo',
-         'estado': 'novo',
-         'jogador_da_vez': '0000',
-         'jogadores': {},
-         'rodadas': 0
+	describe('Testando o estado de um jogo', function() {
+		var path = '/api/';
+		var tabuleiro_nome = 'exemplo';
+
+		it('Teste de resposta 404 do jogo exemplo não criado', function(done) {
+			request(url + path + tabuleiro_nome + '/',
+			  function(error, response, body){
+				(error==null).should.be.true;
+				(response.statusCode).should.be.equal(404);
+				done();
+			});
+		});
+
+		it('Teste de resposta 200 do estado do jogo ao criar um novo jogo', function(done) {
+			var acao = '/novo';
+			request(url + path + tabuleiro_nome + acao,
+			  function(error, response, body) {
+				(error==null).should.be.true;
+				(response.statusCode).should.be.equal(200);
+				done();
+			});
+		});
+
+		it('Teste de resposta do estado do jogo ao criar um novo jogo JSON', function(done) {
+			var acao = '/';
+			var estado_esperado = JSON.stringify({
+		         'nome': 'exemplo',
+		         'estado': 'novo',
+		         'jogador_da_vez': '0000',
+		         'jogadores': {},
+		         'rodadas': 0
 			});
 
-		request(url)
-			.get(path + tabuleiro_nome + acao)
-			.expect(200)
-			.expect(estado_esperado)
-			.end(function(error, response) {
-				if (error) return done(error);
+			request(url + path + tabuleiro_nome + acao,
+			  function(error, response, body){
+				(error==null).should.be.true;
+				(response.statusCode).should.be.equal(200);
+				body.should.be.equal(estado_esperado);
 				done();
-			})
-	});
+			});
+		});
 
-	it('Teste de resposta do estado do jogo finalizado', function(done) {
-		var acao = '/finalizar';
-		var estado_esperado = {
-         'nome': 'exemplo',
-         'estado': 'finalizado',
+		it('Teste de resposta do estado do jogo finalizado', function(done) {
+			var acao = '/finalizar';
+			var estado_esperado = {
+	         	'nome': 'exemplo',
+	         	'estado': 'finalizado',
 			};
 
-		request(url)
-			.get(path + tabuleiro_nome + acao)
-			.expect(200)
-			// .expect(body)
-			.end(function(error, response) {
-				if (error) return done(error);
+			request(url + path + tabuleiro_nome + acao,
+			  function(error, response_head, response_body){
+				(error==null).should.be.true;
+				(response_head.statusCode).should.be.equal(200);
 				done();
 			});
+	// 			// .expect(body)
+				// body.nome.should.be.equal(estado_esperado.nome);
+				// body.estado.should.be.equal(estado_esperado.estado);
+		});
 
-			// body.nome.should.be.equal(estado_esperado.nome);
-			// body.estado.should.be.equal(estado_esperado.estado);
 	});
-
-
 });
