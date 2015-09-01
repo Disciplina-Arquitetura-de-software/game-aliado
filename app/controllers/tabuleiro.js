bcf1311520b27205434904eb29ae24322039c370
Minora, Leonardo <leonardo.minora@gmail.com>
2015-09-01 14:00:28 -0300

    Adicionado a função e o teste para criar tabuleiro.
     Changes to be committed:
    	modified:   app/controllers/tabuleiro.js
    	modified:   test/app/controllers/tabuleiro-test.js

--- app/controllers/tabuleiro.js
+++ app/controllers/tabuleiro.js
@@ -1,22 +1,38 @@
-module.exports = function() {
+module.exports = function(app) {
 	var controller = {};
 	var tabuleiros = [];
+	var Tabuleiro = require('../models/tabuleiro.js');
+	// var Tabuleiro = app.models.tabuleiro;
 
 	controller.recuperar_tabuleiro = function(req, res) {
 
-	// 	for(index in tabuleiros){
-	//         if(tabuleiros[index].nome == req.params.tabuleiro){
-	//             res.status(200).json(tabuleiros[index]);
-	//             return ;
-	//         }
-	//     }
+		var resultados = tabuleiros.filter(function(objeto) {
+			return objeto.nome == req.params.tabuleiro
+		});
+		if (resultados.length) {
+			res.status(200).json(resultados[0]);
+			return;
+		}
+		// for(index in tabuleiros){
+	    //     if(tabuleiros[index].nome == req.params.tabuleiro){
+	    //         res.status(200).json(tabuleiros[index]);
+	    //         return ;
+	    //     }
+	    // }
 
-	    res.status(200).json({erro: "Tabuleiro inexistente"});
-		// res.status(404).json({erro: "Tabuleiro inexistente"});
+		res.status(404).send({
+			resultado: 'erro',
+			erro: 'Tabuleiro inexistente'
+		});
 	};
 
 	controller.criar_tabuleiro = function(req, res) {
-	    res.status(200).send({'erro': 'Tabuleiro inexistente'});
+		var novo = new Tabuleiro({nome: req.params.tabuleiro});
+		tabuleiros.push(novo);
+
+	    res.status(200).send({
+			resultado: 'ok'
+		});
 	};
 
     return controller;
--- test/app/controllers/tabuleiro-test.js
+++ test/app/controllers/tabuleiro-test.js
@@ -10,15 +10,60 @@ describe('API / Tabuleiro : Teste do controlador', function() {
 		var acao = '/';
 
 		var esperado = JSON.stringify({
-			 'erro': 'Tabuleiro inexistente',
+			'resultado': 'erro',
+			 'erro': 'Tabuleiro inexistente'
 		});
 
 		request(url + tabuleiro_nome + acao,
 		  function(error, response, body){
 			(error==null).should.be.true;
-			(response.statusCode).should.be.equal(200);
+			(response.statusCode).should.be.equal(404);
 			body.should.be.equal(esperado);
 			done();
+		});
+	});
+
+	describe('CRIAR Teste de criar novo tabuleiro', function() {
+		it('\n\tTestando o resultado da chamada', function(done) {
+			var acao = '/criar';
+
+			var esperado = JSON.stringify({
+				 'resultado': 'ok',
+			});
+
+			request(url + tabuleiro_nome + acao,
+			  function(error, response, body){
+				(error==null).should.be.true;
+				(response.statusCode).should.be.equal(200);
+				body.should.be.equal(esperado);
+				done();
+			});
+		});
+
+		it('\n\tTestando o tabuleiro JSON após a criação', function(done) {
+			var acao = '/criar';
+
+			var esperado = {
+					nome: 'exemplo',
+					estado: 'novo',
+					jogador_da_vez: '0000',
+					jogadores: {},
+					rodadas: 0
+			};
+
+			request(url + tabuleiro_nome + acao,
+			  function(error, response, body){});
+
+			request(url + tabuleiro_nome + '/',
+			  function(error, response, body){
+				(error==null).should.be.true;
+				(response.statusCode).should.be.equal(200);
+				estado_computado = JSON.parse(body);
+				estado_computado.should.have
+					.properties(esperado);
+				// body.should.be.equal(esperado);
+				done();
+			});
 		});
 	});
 
